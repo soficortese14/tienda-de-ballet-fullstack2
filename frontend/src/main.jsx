f212b4587c865f069c19d3771b8cdd15bdc9a570
Sofi <sof.cortese@duocuc.cl>
2026-01-25 11:59:57 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,6 @@
+//1. Buscar el elemento HTML con id="root" (está en index.html)                                                                                      
+//2. Renderizar (mostrar) el componente <App /> dentro de ese elemento                                                                               
+//3. Activar StrictMode de React (modo estricto para detectar problemas) 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
